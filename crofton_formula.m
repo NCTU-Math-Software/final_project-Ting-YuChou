d86d00c90994f6dfbd6ef54b641f6f594fb7fc33
Ting-YuChou <71478757+Ting-YuChou@users.noreply.github.com>
2020-12-20 22:14:17 +0800

    Add files via upload

---
+++ crofton_formula.m
@@ -0,0 +1,69 @@
+
+function crofton_formula(R)
+    %input: R,radius
+    %output: length of circle
+    %example: crofton_formula(1)
+    
+    %crofton formula
+    fun= @(x,y) 2+zeros(size(x));
+    Len=1/2*integral2(fun,0,2*pi,0,R);
+    disp(Len)
+
+    
+    %general way of approximation
+    n=0;
+    p=linspace(0,R);
+    theta=linspace(0,pi,181);
+    x=linspace(-R,R,10^4+1);
+    
+    for ii=2:180
+        for jj=2:99
+            xp=p(jj)*cos(theta(ii));
+            yp=p(jj)*sin(theta(ii));
+            c=xp^2+yp^2;
+            
+            for kk=1:10^4
+                if f(x(kk))*f(x(kk+1))<0
+                    n=n+1;
+                end
+            end
+        
+        end
+    end
+    
+    n=n*2;
+    
+    for ii=2:90
+        k=sin(theta(ii))/cos(theta(ii));
+        for kk=1:10^4
+            if g(x(kk))*g(x(kk+1))<0
+                n=n+1;
+            end
+        end
+    end
+    
+    for ii=92:180
+        k=sin(theta(ii))/cos(theta(ii));
+        for kk=1:10^4
+            if g(x(kk))*g(x(kk+1))<0
+                n=n+1;
+            end
+        end
+    end
+    
+    n=n+360+98*2*2;
+    disp(n)             %total number of points
+    
+    function y=f(x)
+        y=(1+(xp/yp)^2)*x^2-(2*xp*c/(yp^2))*x+(c/yp)^2-R^2;
+    end
+    
+    
+    function y=g(x)
+        y=(1+k^2)*x^2-R^2;
+    end
+
+    curve_length=1/2*n*pi/180*R/99;
+    disp(curve_length)
+
+end
